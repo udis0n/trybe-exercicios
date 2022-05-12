66aaecfb0af2e8c396a027b016de1460c0492cf8
Udison Abreu <udi0abreu@gmail.com>
2022-05-12 19:10:43 -0300

    adicionado programas de comparação js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/compare2.js
@@ -0,0 +1,10 @@
+const a = 10;
+const b = 10;
+
+if (a > b) {
+    console.log("O maior número é " + a +".");
+} else if (b > a) {
+    console.log("O maior número é " + b +".");
+} else {
+    console.log("Os dois números são iguais!");
+};

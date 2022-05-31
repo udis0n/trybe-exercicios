993102a01cb41044de438e4cf103d6657334ecb2
Udison Abreu <udi0abreu@gmail.com>
2022-05-31 19:55:18 -0300

    exercises day 1

---
+++ fundamentos/bloco-6-html-e-css-forms-flexvox-e-responsivo/dia-1-html-e-css-forms/script.js
@@ -0,0 +1,10 @@
+const btnSubmitForm = document.querySelector('#btn-submit-form');
+const checkbox2 = document.querySelector('#checkbox2');
+
+console.log(btnSubmitForm);
+
+// function prevent(event) {
+//     event.preventDefault();
+// }
+
+// btnSubmitForm.addEventListener('click', prevent);

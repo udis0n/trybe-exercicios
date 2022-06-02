5caa85b0e4200b70dc3992299cb60fb5bc412202
Udison Abreu <udi0abreu@gmail.com>
2022-06-02 15:24:21 -0300

    day 6.2

---
+++ fundamentos/bloco-6-html-e-css-forms-flexvox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,17 @@
+const picker = new Pikaday({
+    field: document.getElementById('date'),
+    format: 'D/M/YYYY',
+    toString(date, format) {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+        const parts = dateString.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+        return new Date(year, month, day);
+    }
+  });

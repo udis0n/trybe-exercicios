7e29535599da391620ac7fd9b4d4255393a6c595
Udison Abreu <udi0abreu@gmail.com>
2022-06-28 19:14:17 -0300

    exercise9

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise9.js
@@ -0,0 +1,13 @@
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const {spring, summer, autumn, winter} = yearSeasons;
+
+yearMonths = [...winter, ...spring, ...summer, ...autumn];
+
+console.log(yearMonths);
